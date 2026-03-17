46b21c9ca05abab1e26a7e0f8f9f4f4332a630da
lvadegre <lvadegre>
2026-03-17 15:31:12 +0100

    add instance of hero in GuiMain

--- new_game/src/application/GuiMain.java
+++ new_game/src/application/GuiMain.java
@@ -3,13 +3,29 @@ package application;
 import entity.view.gui.HeroViewGUI;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import entity.model.HeroModel;
-
+import common.entity.Hero;
+import common.item.Bag;
+import common.map.Room;
 
 public class GuiMain extends Application {
+
+    private final static int DEFAULT_HERO_DAMAGE = 10;
+    private final static int DEFAULT_HERO_BAG_CAPACITY = 5;
+
     @Override
     public void start(Stage stage) {
         HeroViewGUI gui = new HeroViewGUI(stage);
+
+        Room entrance = new Room("Entrance");
+
+        Hero hero = new Hero(
+                "Hero",
+                100,
+                new Bag(
+                        "nom du sac",
+                        DEFAULT_HERO_BAG_CAPACITY),
+                entrance,
+                DEFAULT_HERO_DAMAGE);
 
         gui.setHeroName("Hero");
         gui.showLocation("Entrance");
